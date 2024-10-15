99cc8f778b815d9a4538fe07998744c34c3dbc85
JungHyeonKim <dev.kjung@gmail.com>
2024-10-15 23:20:24 +0900

    [Feat]: create project

---
+++ src/main/java/com/example/querydlspractice/QuerydlsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.querydlspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydlsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydlsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/querydlspractice/QuerydlsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydlspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydlsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
