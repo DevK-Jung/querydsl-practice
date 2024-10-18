175e716c97694ffb829dba28cec2bcc840ad7441
JungHyeonKim <dev.kjung@gmail.com>
2024-10-18 21:01:16 +0900

    [Feat]: 예제 도메인 모델 생성 및 동작 테스트

---
+++ src/main/java/com/example/querydlspractice/member/entity/Member.java
@@ -0,0 +1,43 @@
+package com.example.querydlspractice.member.entity;
+
+import com.example.querydlspractice.team.entity.Team;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0, null);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            chagneTeam(team);
+        }
+    }
+
+    private void chagneTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
