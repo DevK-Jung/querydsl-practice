5dd52954d982a461dcc0ccbfea85ce1874ddb0c0
JungHyeonKim <dev.kjung@gmail.com>
2024-11-20 23:38:10 +0900

    [Feat]: querydls sort 테스트

--- src/test/java/com/example/querydlspractice/QuerydlsBasicTest.java
+++ src/test/java/com/example/querydlspractice/QuerydlsBasicTest.java
@@ -97,4 +97,32 @@ public class QuerydlsBasicTest {
                 .selectFrom(member)
                 .fetchFirst();
     }
+
+    /**
+     * <h1>정렬 순서</h1>
+     *<ul>
+     *     <li>회원 나이 내림차순</li>
+     *     <li>회원 이름 내림차순 - 단 이름이 없으면(null) 마지막에 출력</li>
+     *</ul>
+     */
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> results = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsFirst())
+                .fetch();
+
+        Member memberNull = results.get(0); // 이름이 null
+        Member member5 = results.get(1);
+        Member member6 = results.get(2);
+
+        Assertions.assertThat(memberNull.getUsername()).isNull();
+        Assertions.assertThat(member5.getUsername()).isEqualTo("member5");
+        Assertions.assertThat(member6.getUsername()).isEqualTo("member6");
+    }
 }
