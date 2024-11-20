d12924494baf89dc6110d396fa3f6f975d97871f
JungHyeonKim <dev.kjung@gmail.com>
2024-11-20 23:16:23 +0900

    [Feat]: member1 찾기 테스트 코드 작성 JPQL 과 QueryDsl 비교

--- src/main/java/com/example/querydlspractice/sample/entity/Hello.java
+++ src/main/java/com/example/querydlspractice/sample/entity/Hello.java
@@ -3,12 +3,15 @@ package com.example.querydlspractice.sample.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Hello {
     @Id
     @GeneratedValue
---
+++ src/test/java/com/example/querydlspractice/QuerydlsBasicTest.java
@@ -0,0 +1,69 @@
+package com.example.querydlspractice;
+
+import com.example.querydlspractice.member.entity.Member;
+import com.example.querydlspractice.member.entity.QMember;
+import com.example.querydlspractice.team.entity.Team;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.querydlspractice.member.entity.QMember.member;
+
+@Transactional
+@SpringBootTest
+public class QuerydlsBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    // 테스트 케이스 실행전 테스트 데이터 세팅
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        String qlString =
+                "select m from Member m where m.username = :username";
+        // member1 찾기
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQueryDsl() {
+
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
