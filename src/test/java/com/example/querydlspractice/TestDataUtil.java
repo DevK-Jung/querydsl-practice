8daaba2cae9ceec3d50be3a933fc1ff436af70ab
JungHyeonKim <dev.kjung@gmail.com>
2024-11-21 21:31:06 +0900

    [Feat]: Test data 별도 메서드로 분리

---
+++ src/test/java/com/example/querydlspractice/TestDataUtil.java
@@ -0,0 +1,25 @@
+package com.example.querydlspractice;
+
+import com.example.querydlspractice.member.entity.Member;
+import com.example.querydlspractice.team.entity.Team;
+import jakarta.persistence.EntityManager;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TestDataUtil {
+    public void setupTestData(EntityManager em) {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+}
---
+++ src/test/java/com/example/querydlspractice/sort/QuerydslSortTest.java
@@ -0,0 +1,63 @@
+package com.example.querydlspractice.sort;
+
+import com.example.querydlspractice.TestDataUtil;
+import com.example.querydlspractice.member.entity.Member;
+import com.example.querydlspractice.team.entity.Team;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.example.querydlspractice.member.entity.QMember.member;
+
+@Transactional
+@SpringBootTest
+public class QuerydslSortTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    // 테스트 케이스 실행전 테스트 데이터 세팅
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+        TestDataUtil.setupTestData(em);
+    }
+
+
+    /**
+     * <h1>정렬 순서</h1>
+     * <ul>
+     *     <li>회원 나이 내림차순</li>
+     *     <li>회원 이름 내림차순 - 단 이름이 없으면(null) 마지막에 출력</li>
+     * </ul>
+     */
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> results = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsFirst())
+                .fetch();
+
+        Member memberNull = results.get(0); // 이름이 null
+        Member member5 = results.get(1);
+        Member member6 = results.get(2);
+
+        Assertions.assertThat(memberNull.getUsername()).isNull();
+        Assertions.assertThat(member5.getUsername()).isEqualTo("member5");
+        Assertions.assertThat(member6.getUsername()).isEqualTo("member6");
+    }
+}
