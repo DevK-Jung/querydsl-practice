7bc8f29ea550f5b65815ff2cdb99c405fa5ef64e
JungHyeonKim <dev.kjung@gmail.com>
2024-11-21 21:50:38 +0900

    [Feat]: join 테스트

---
+++ src/test/java/com/example/querydlspractice/basic/JoinTest.java
@@ -0,0 +1,64 @@
+package com.example.querydlspractice.basic;
+
+import com.example.querydlspractice.TestDataUtil;
+import com.example.querydlspractice.member.entity.Member;
+import com.example.querydlspractice.team.entity.QTeam;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.example.querydlspractice.member.entity.QMember.member;
+import static com.example.querydlspractice.team.entity.QTeam.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+public class JoinTest {
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+        TestDataUtil.setupTestData(em);
+    }
+
+    @Test
+    public void join() {
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    @Test
+    public void thetaJoin() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
+}
