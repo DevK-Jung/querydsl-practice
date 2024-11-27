a2190166ec33b1d282488e7fc0c22f4b0100e26f
JungHyeonKim <dev.kjung@gmail.com>
2024-11-27 23:26:28 +0900

    [Feat]: 기본문법 join on 절 테스트

--- src/test/java/com/example/querydlspractice/basic/JoinTest.java
+++ src/test/java/com/example/querydlspractice/basic/JoinTest.java
@@ -2,11 +2,11 @@ package com.example.querydlspractice.basic;
 
 import com.example.querydlspractice.TestDataUtil;
 import com.example.querydlspractice.member.entity.Member;
-import com.example.querydlspractice.team.entity.QTeam;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static com.example.querydlspractice.member.entity.QMember.member;
-import static com.example.querydlspractice.team.entity.QTeam.*;
+import static com.example.querydlspractice.team.entity.QTeam.team;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
@@ -60,5 +60,59 @@ public class JoinTest {
         assertThat(result)
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
+    }
+
+    /**
+     * 회원과 팀을 조인하면서, 팀 이름이 teamA 인 팀만 조인, 회원은 모두 조회
+     * JPQL: select m from Member m left join m.team t on t.name = "teamA"
+     */
+    @Test
+    public void joinOnFiltering() {
+
+        // 아래 두 쿼리는 동일
+//        List<Tuple> result = queryFactory
+//                .select(member, team)
+//                .from(member)
+//                .join(member.team)
+//                .on(team.name.eq("teamA"))
+//                .fetch();
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .join(member.team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+
+    }
+
+    /**
+     * 연관 관계 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    @DisplayName("연관 관계 없는 엔티티 외부 조인")
+    public void joinOnNoRelation() {
+
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+//                .leftJoin(member.team, team) // 이렇게 하면 아이디가 자동 조인됨
+                .leftJoin(team)
+                .on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+
     }
 }
